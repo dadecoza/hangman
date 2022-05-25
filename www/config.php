29e8c2a19a4680dec8a37180746a008a13812c2a
dadecoza <dade@dade.co.za>
2022-05-25 14:07:13 +0200

    example config

--- www/config.php
+++ www/config.php
@@ -1,8 +1,8 @@
 <?php
 return array(
     'servername' => 'localhost',
-    'database' => 'langebe2_hangman',
-    'username' => 'langebe2_hangman',
-    'password' => 'aB%2K[tBMVN='
+    'database' => 'hangman',
+    'username' => 'hangman',
+    'password' => 'hangm4n'
 );
 ?>
